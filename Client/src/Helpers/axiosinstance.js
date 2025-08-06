a343c5282567a3a335046bd2a02a2bfa62492f14
Aditya Maurya <adityamauryanitt@gmail.com>
2025-08-07 01:28:16 +0530

    ..

--- Client/src/Helpers/axiosinstance.js
+++ Client/src/Helpers/axiosinstance.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 // Environment-based BASE_URL (ensure VITE_API_BASE_URL is set in your `.env`)
-const BASE_URL = "https://lms-sandy-psi.vercel.app/api/v1";
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 console.log("VITE_API_BASE_URL:", import.meta.env.VITE_API_BASE_URL);
 const axiosInstance = axios.create({
-  baseURL: BASE_URL,
   withCredentials: true,
 });
 

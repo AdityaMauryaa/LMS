e67e7bb01f72642f768769cf7c6ce05ba137d698
Aditya Maurya <adityamauryanitt@gmail.com>
2025-08-07 01:29:21 +0530

    renamed url

--- Client/src/Helpers/axiosinstance.js
+++ Client/src/Helpers/axiosinstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Environment-based BASE_URL (ensure VITE_API_BASE_URL is set in your `.env`)
 axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
-console.log("VITE_API_BASE_URL:", import.meta.env.VITE_API_BASE_URL);
+console.log("VITE_API_BASE_URL:", import.meta.env.VITE_BACKEND_URL);
 const axiosInstance = axios.create({
   withCredentials: true,
 });
